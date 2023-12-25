c6cb27c58e35461527167ef86c83ec416573c61c
李嘉俊 <2383690354@qq.com>
2023-12-25 14:57:53 +0800

    先这样吧，真觉得我写的这个好烂

--- Old8Lang.App/Program.cs
+++ Old8Lang.App/Program.cs
@@ -1,10 +1,10 @@
 ﻿using Old8Lang;
 
 //APIs.ChangeBasicInfo("/home/luckyfish/RiderProjects/Old8Lang/Old8LangLib/OldLib/","0.3.0");
-string[] strings = { "-f" ,Path.Combine(Path.GetDirectoryName(BasicInfo.CodePath)!,"Old8Lang","Ex", "init.ws")};
+string[] strings = ["-f", Path.Combine(Path.GetDirectoryName(BasicInfo.CodePath)!, "Old8Lang", "Ex", "init.ws")];
 //
 
-args = args.Length == 0?strings:args;
+args = args.Length == 0 ? strings : args;
 if (args.Length == 0)
     args = Console.ReadLine()!.Split(" ");
 
@@ -17,44 +17,48 @@ void Lang(string[] oder)
     var a = Apis.ReadJson();
     if (oder[0] == BasicInfo.Order["FromFile"])
     {
-        Run(false,oder[1]);
+        Run(false, oder[1]);
         return;
     }
+
     if (oder[0] == BasicInfo.Order["FromDir"])
     {
-        Run(true,oder[1]);
+        Run(true, oder[1]);
         return;
     }
+
     if (oder[0] == BasicInfo.Order["Import"])
     {
         foreach (var VARIABLE in a.LibInfos)
             Console.WriteLine($"LibName:{VARIABLE.LibName} Var:{VARIABLE.Var} IsDir:{VARIABLE.IsDir}");
-        Console.WriteLine("in:"+a.ImportPath);
+        Console.WriteLine("in:" + a.ImportPath);
         return;
     }
+
     if (oder[0] == BasicInfo.Order["ChangeImport"])
     {
-        var b = Apis.ChangeBasicInfo(oder[1],a.Ver);
-        Console.WriteLine("now:"+b.ImportPath);
+        var b = Apis.ChangeBasicInfo(oder[1], a.Ver);
+        Console.WriteLine("now:" + b.ImportPath);
         return;
     }
+
     if (oder[0] == BasicInfo.Order["Var"])
     {
         Console.WriteLine(a.Ver);
         return;
     }
+
     if (oder[0] == BasicInfo.Order["Info"])
     {
         Console.WriteLine(BasicInfo.Info());
     }
 }
 
-void Run(bool isdic,string path)
+void Run(bool isdic, string path)
 {
-    var info = Apis.CslyUsing(path,isdic);
+    var info = Apis.CslyUsing(path, isdic);
     info.Error.ForEach(Console.WriteLine);
-    Console.WriteLine("\n"+info.Time);
+    Console.WriteLine("\n" + info.Time);
 }
-
 
 #endregion
