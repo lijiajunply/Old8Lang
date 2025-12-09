31a50e137bc79a3503bd066516b6457e2d059d9d
LuckyFish at macOS <2383690354@qq.com>
2025-12-10 02:45:55 +0800

    feat(错误处理): 为错误信息添加颜色高亮显示
    使用 ANSI 转义码为错误信息添加红色和黄色高亮
    显示错误代码和位置信息，提高可读性

--- Old8Lang/AST/Expression/Intermediates/ErrorLangValue.cs
+++ Old8Lang/AST/Expression/Intermediates/ErrorLangValue.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Old8Lang.Error;
 
 namespace Old8Lang.AST.Expression.Intermediates;
@@ -8,5 +9,16 @@ namespace Old8Lang.AST.Expression.Intermediates;
 /// <param name="value"></param>
 public class ErrorLangValue(Old8Exception value) : LangValueType
 {
-    public override string ToString() => $"{value.Message}";
+    public override string ToString()
+    {
+        const string reset = "\u001b[0m";
+        const string red = "\u001b[31m";
+        const string yellow = "\u001b[33m";
+        StringBuilder sb = new();
+
+        sb.AppendLine($"{red}Old8Lang Error: {value.ErrorCode}{reset}");
+        sb.AppendLine($"{yellow}{value.Position}{reset}");
+
+        return sb.ToString();
+    }
 }
